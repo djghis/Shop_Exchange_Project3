92d8d2877ffcc87371e4b12eae39abf3a0080647
JohnHG1 <honisz@yahoo.com>
2020-04-25 20:03:52 +0100

    Added Model Category

---
+++ src/main/java/com/codeclan/example/Shop_Exchange/ShopExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.Shop_Exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopExchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/example/Shop_Exchange/ShopExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.Shop_Exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
