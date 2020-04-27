2372ff793548f0e53262f4a1fffb86bdc4e0abfb
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-04-27 14:54:38 +0100

    Initial React set up files complete

---
+++ shop_exchange/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Homepage from './containers/Homepage';
+
+function App() {
+  return (
+    <Homepage/>
+  );
+}
+
+export default App;
---
+++ shop_exchange/src/containers/Homepage.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export class Homepage extends Component {
+    
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Homepage
