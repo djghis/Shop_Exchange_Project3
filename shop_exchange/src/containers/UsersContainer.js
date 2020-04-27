502c6e10ffdd3a0baeeb5e96336cdfb3953f26ee
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-04-27 15:27:30 +0100

    Backup

---
+++ shop_exchange/src/containers/UsersContainer.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import Request from '../helpers/request';
+
+
+export class UsersContainer extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+             users:[]
+        }
+    }
+
+    componentDidMount() {
+        const request = new Request()
+        request.get('/api/users')
+        .then((data)=>{
+            this.setState({
+                users:data
+            })
+        })
+
+
+
+    }
+    
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default UsersContainer
