45364b067f6d0210a9f02fa8e761463675d49b7b
Gmorgan87 <gmorgan87@hotmail.co.uk>
2020-04-28 14:46:36 +0100

    adds details to productDetail

--- shop_exchange/src/components/ProductDetails.js
+++ shop_exchange/src/components/ProductDetails.js
@@ -4,9 +4,14 @@ const ProductDetails = (props) => {
     console.log(props);
     
   return (
-    <div>
-      <span>{props.product.name} </span>
-      <span>qty:{props.product.qty}</span>
+    <div className="product-details">
+      <p>name: {props.product.name} </p>
+      <p>desc: {props.product.description} </p>
+      <p>category: {props.product.category.name} </p>
+      <p>status: {props.product.status} </p>
+      <p>rent noditions: {props.product.rentCodnitions} </p>
+      <p>replace status: {props.product.replaceStatus} </p>
+      <p>qty:{props.product.qty}</p>
     </div>
   )
 }
